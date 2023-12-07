4ad579d97fc5e13be9d7652ff0d92727b78b53ff
topanekka <126932631+topanekka@users.noreply.github.com>
2023-12-07 19:16:00 +0530

    Add files via upload

---
+++ play.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="css/bootstrap-reboot.min.css"/>
+	<link rel="stylesheet" href="css/bootstrap-grid.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/slider-radio.css"/>
+	<link rel="stylesheet" href="css/select2.min.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/main.css"/>
+	<link rel="shortcut icon" href="favicon.png"/>
+	<title>Play TV | Airtel Xstream Online</title>
+	<script type='text/javascript' src='https://content.jwplatform.com/libraries/IDzF9Zmk.js'></script>
+    <script type="text/javascript">jwplayer.key = 'Khpp2dHxlBJHC8MCmLnBuV2jK/DwDnJMniwF6EO9HC/riJ712ZmbHg==';</script>
+</head>
+
+<body>
+	<!-- header (hidden style) -->
+	<header class="header header--hidden">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="header__content">
+
+						<a href="index.php" class="header__logo">
+							<img src="logo.png" alt="Watch Airtel XStream Channels Live Here" />
+						</a>
+
+						<ul class="header__nav"></ul>
+
+						<div class="header__actions">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- end header -->
+
+	<!-- details -->
+	<section class="section section--head section--head-fixed section--gradient section--details-bg">
+		<div class="section__bg" data-bg="img/netimi.jpg"></div>
+		<div class="container">
+			<!-- article -->
+			<div class="article">
+				<div class="row">
+					<div class="col-12 col-xl-8">
+						
+						<!-- article content -->
+						<div class="article__content">
+							<h1 id="stv_title"></h1>
+							<ul class="list">
+								<li id="stv_category"></li>
+								<li id="stv_language"></li>
+							</ul>
+						</div>
+						<!-- end article content -->
+
+					</div>
+
+					<!-- video player -->
+					<div class="col-12 col-xl-8">
+						
+						<!--Player Here-->
+						<div class="container">
+    						<div id="vplayer" style="height: auto; text-align: center;"></div>
+    					</div>
+						<!--//Player Here-->
+	
+					</div>
+					<!-- end video player -->
+
+					<div class="col-12 col-xl-8" id="external_player_interface" style="display: none;">
+						
+						<div class="categories">
+							<h3 class="categories__title">Unable To Stream On This Player ? Try Native Player Below</h3>
+							<a href="" id="andr_mxfree" class="categories__item">MX Player (Free)</a>
+							<a href="" id="andr_mxpro" class="categories__item">MX Player (Pro)</a>
+							<a href="" id="window_ottplayer" class="categories__item">Window Users (Pot Player)</a>
+						</div>
+						
+						<div class="categories">
+						<h3 class="categories__title">Install Native Players For Better PlayBack</h3>
+							<a href="https://apkfinder.xyz/down/mx%20player/MX%20Player_MOD_v1.48.5-Ytricks.net.apk" target="_blank" class="categories__item">Download MX Player For Android</a>
+							<a href="https://potplayer.daum.net/" target="_blank" class="categories__item">Download Pot Player For Windows</a>
+						</div>
+
+					</div>
+    
+    <script>var channel_id = "<?php if(isset($_GET['id'])){ print($_GET['id']); } ?>";</script>
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/slider-radio.js"></script>
+	<script src="js/select2.min.js"></script>
+	<script src="js/smooth-scrollbar.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/app.js?v=<?php print(time()); ?>" onload="load_tv_detail()"></script>
+	
+</body>
+</html>
